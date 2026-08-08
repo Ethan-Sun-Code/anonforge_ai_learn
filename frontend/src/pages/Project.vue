d749502f839724b6876e73af96eb4e264fda029d
Guowen Sun <govensun53@gmail.com>
2026-08-08 09:13:26 +0100

    feature: 项目初始化

---
+++ frontend/src/pages/Project.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <el-button type="success" disabled>Success</el-button>
+        项目管理界面
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Project"
+}
+</script>
